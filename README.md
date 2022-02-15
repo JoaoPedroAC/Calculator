8702b150d81ee4b73494feadccd971b7308357c6
João Pedro de Almeida <joaopedro.alm.c@gmail.com>
2022-02-14 22:13:07 -0300

    consegui fazer o C functionar

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # SOBRE O PROJETO
+
 Aprendi sobre `function factory` no curso de JS e resolvi praticar crinado uma calculadora.
 
 ## TECNOLOGIAS USADAS
+
 - HTML;
 - CSS;
 - JavaScript.
+
+## DEPLOY
+
+[Página aqui!](https://joaopedroac.github.io/Calculator/).
--- asset/Script/script.js
+++ asset/Script/script.js
@@ -2,57 +2,36 @@ function calculator() {
 	let buttons = document.querySelectorAll('button');
 	// const button = document.querySelector('button');
 	const screen = document.querySelector('.screen');
+	const array = [];
 
-	function actions(button = '', symbols = '',acumulator = '') {
+	function actions() {
 		return {
-			button,
-			symbols,
-			acumulator,
 			clean() {
-				if (symbols === 'C') screen.innerHTML = '';
-				// button !== 'C' ? (screen.innerHTML += button) : (screen.innerHTML = '');
+				if (array.includes('C')) return (array.length = 0);
+			},
+			printing() {
+				screen.innerHTML = array.join('');
 			},
 			count() {
-				if (this.symbols === '+') {
-					const soma = this.button + this.acumulator;
-					screen.innerHTML = `${this.button} + ${this.button}`;
-					return soma;
-				}
+				// deixei dando erro mesmo
+				let conta = array.join(parseInt(array));
+				return conta;
 			},
 			igual() {
-				let result = count();
-				return (screen.innerHTML = result);
+				if (array.includes('=')) {
+					let result = this.count();
+					screen.innerHTML = result;
+					array.length = 0;
+				}
 			},
 		};
 	}
 	for (let botoes of buttons) {
 		botoes.addEventListener('click', function () {
-			let valor = botoes.value;
-			let numercis = actions().button;
-			numercis =
-				valor !== '+' ||
-				valor !== '-' ||
-				valor !== '/' ||
-				valor !== '*' ||
-				valor !== '**' ||
-				valor !== '.' ||
-				valor !== 'C'
-					? valor
-					: '';
-			let operador = actions().symbols;
-			operador =
-				valor === '+' ||
-				valor === '-' ||
-				valor === '/' ||
-				valor === '*' ||
-				valor === '**' ||
-				valor === '.' ||
-				valor === 'C'
-					? valor
-					: '';
-			screen.innerHTML += valor
-			actions(numercis, operador).clean();
-			actions(numercis, operador).count();
+			array.push(botoes.value);
+			actions().clean();
+			actions().printing();
+			actions().igual();
 		});
 	}
 }
