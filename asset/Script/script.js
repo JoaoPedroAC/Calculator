4559517f32e682d78fa0f4b66051387992636147
João Pedro de Almeida <joaopedro.alm.c@gmail.com>
2022-02-15 22:30:55 -0300

    identei o codigo e add outras funcioanlidades

--- asset/Script/script.js
+++ asset/Script/script.js
@@ -2,33 +2,50 @@ function calculator() {
 	let buttons = document.querySelectorAll('button');
 	// const button = document.querySelector('button');
 	const screen = document.querySelector('.screen');
+	// aqui é onde armazeno os valores que serao exibidos no HTML
 	const array = [];
 
 	function actions() {
 		return {
+			// funcao para limpar os numeros
 			clean() {
 				if (array.includes('C')) return (array.length = 0);
+				if (array.includes('<<')) {
+					return (array.length -= 2);
+				}
 			},
+			// para escrever no HTML
 			printing() {
 				screen.innerHTML = array.join('');
 			},
+			// area das contas
+			// ainda ta dando um problema que ele não soma
 			count() {
-				// deixei dando erro mesmo
-				let conta = array.join(parseInt(array));
+				array.pop();
+				let acumulator = array.join('');
+				let conta = parseInt(acumulator);
 				return conta;
 			},
+			// ao apertar o botao '=' sera efetuado a conta e aparecerá na tela, fora q tbm salvara no array
 			igual() {
 				if (array.includes('=')) {
 					let result = this.count();
 					screen.innerHTML = result;
 					array.length = 0;
+					array.push(result);
 				}
 			},
 		};
 	}
+	// area dos botoes e seus respectivos valores, aqui eh onde tudo funciona
 	for (let botoes of buttons) {
 		botoes.addEventListener('click', function () {
-			array.push(botoes.value);
+			// essa const é para caso de NaN ele retornará o os valores nao numericos
+			const errors = !parseInt(botoes.value)
+				? botoes.value
+				: parseInt(botoes.value);
+			array.push(errors);
+			// declaracao das functions
 			actions().clean();
 			actions().printing();
 			actions().igual();
